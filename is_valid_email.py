fd63b0f1bd35eb55c741bde9d2a064fdaad062d5
abhinavkrishna006 <137138307+abhinavkrishna006@users.noreply.github.com>
2023-11-19 10:23:25 +0530

    Update is_valid_email.py

--- is_valid_email.py
+++ is_valid_email.py
@@ -1,16 +1,33 @@
-import re
-
 def is_valid_email(email):
-    # Define a regular expression for a basic email pattern
-    email_pattern = re.compile(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+    # Rule 1: Must contain at least 1 character before the @ symbol
+    if email.find('@') == 0:
+        return False
 
-    # Use the regular expression to match the email pattern
-    match = re.match(email_pattern, email)
+    # Rule 2: Must contain the @ symbol
+    if '@' not in email:
+        return False
 
-    # If there's a match, the email is valid; otherwise, it's not valid
-    return bool(match)
+    # Rule 3: Must have at least 1 character after the last period (.)
+    if email.rfind('.') == len(email) - 1:
+        return False
+
+    # Rule 4: Must contain at least 1 character after the last period (.)
+    if email[email.rfind('.') + 1:] == '':
+        return False
+
+    # Rule 5: Maximum 256 characters
+    if len(email) > 256:
+        return False
+
+    # Rule 6: Must start with a letter or a number
+    if not (email[0].isalpha() or email[0].isdigit()):
+        return False
+
+    # If all rules pass, the email is valid
+    return True
 
 # Example usage:
 email_address = "example@email.com"  # Replace with the email address you want to check
 result = is_valid_email(email_address)
 print(f"Is the email address {email_address} valid? {result}")
+
